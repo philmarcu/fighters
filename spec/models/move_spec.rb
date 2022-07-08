fc8d89cba2c3700361cb9c7aa220a55b2331c535
philmarcu <philmark18@gmail.com>
2022-07-07 18:05:11 -0600

    Feat- complete model tests

--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Move, type: :model do
     it { should validate_presence_of :power }
     it { should validate_presence_of :speed }
     it { should validate_presence_of :grade }
+    it { should allow_value(true).for(:top_tier)}
+    it { should allow_value(false).for(:top_tier)}
   end
 
   describe 'relationships' do
