05aadce8c65cfeae24baf8a62289faa080b9fc5f
philmarcu <philmark18@gmail.com>
2022-07-10 11:37:55 -0600

    Feat- create update action for Fighters table

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -22,4 +22,20 @@ class FightersController < ApplicationController
     fighter.save
     redirect_to '/fighters'
   end
+
+  def edit
+    @fighter = Fighter.find(params[:id])
+  end
+
+  def update
+    fighter = Fighter.find(params[:id])
+    fighter.update({
+      name: params[:fighter][:name],
+      rival: params[:fighter][:rival],
+      rank: params[:fighter][:rank],
+      style: params[:fighter][:style]
+    })
+    fighter.save
+    redirect_to "/fighters/#{fighter.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get '/fighters', to: 'fighters#index'
   get 'fighters/new', to: 'fighters#new'
   post '/fighters', to: 'fighters#create'
-  get '/moves', to: 'moves#index'
   get '/fighters/:id', to: 'fighters#show'
+  get '/fighters/:id/edit', to: 'fighters#edit'
+  patch '/fighters/:id', to: 'fighters#update'
+  get '/moves', to: 'moves#index'
   get '/moves/:id', to: 'moves#show'
   get '/fighters/:fighter_id/moves', to: 'fighter_moves#index'
 end
