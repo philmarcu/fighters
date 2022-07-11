37f89626f9362732aaff0ca3bcac0126f6cd5ce1
philmarcu <philmark18@gmail.com>
2022-07-11 13:11:58 -0600

    Feat- complete GET & POST routes for fighter_moves#new and fighter_moves#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/fighters', to: 'fighters#index'
+  get '/moves', to: 'moves#index'
+  get '/moves/:id', to: 'moves#show'
   get 'fighters/new', to: 'fighters#new'
   post '/fighters', to: 'fighters#create'
   get '/fighters/:id', to: 'fighters#show'
   get '/fighters/:id/edit', to: 'fighters#edit'
   patch '/fighters/:id', to: 'fighters#update'
-  get '/moves', to: 'moves#index'
-  get '/moves/:id', to: 'moves#show'
-  get '/fighters/:id/moves', to: 'fighter_moves#index'
-  get '/fighters/:id/moves/new', to: 'fighter_moves#new'
-  post '/fighters/:id/moves', to: 'fighter_moves#create'
+  get '/fighters/:fighter_id/moves', to: 'fighter_moves#index'
+  get '/fighters/:fighter_id/moves/new', to: 'fighter_moves#new'
+  post '/fighters/:fighter_id/moves', to: 'fighter_moves#create'
+  
 end
