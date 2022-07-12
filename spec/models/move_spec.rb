ff7ff25599151ec8ec1c9677832a76f40a010724
philmarcu <philmark18@gmail.com>
2022-07-12 11:27:22 -0600

    Test- complete passing top tier test for User Story 15

--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe Move, type: :model do
   describe 'relationships' do
     it { should belong_to :fighter}
   end
+
+  describe 'top tier' do
+    it 'goes to the top tier page' do
+      fox = Fighter.create!(name: "Fox The Boxer", rival: "true", rank: 3, style: "Paw-Boxing")
+      turtle = Fighter.create!(name: "The Tilted Turtle", rival: "false", rank: 5, style: "Ninjutsu")
+
+      move = fox.moves.create!(name: "Slash (Light)", power: 41, speed: 93, grade: "77.0 - B Tier", top_tier: false, fighter_id: fox.id)
+      move2 = turtle.moves.create!(name: "Roundhouse (Heavy)", power: 60, speed: 62, grade: "71.0 - B Tier", top_tier: false, fighter_id: turtle.id)
+      top1 = fox.moves.create!(name: "SuperSlash (Special)", power: 99, speed: 99, grade: "99.9 - S Tier", top_tier: true, fighter_id: fox.id)
+      top2 = turtle.moves.create!(name: "Big Boom (Special)", power: 99, speed: 99, grade: "99.9 - S Tier", top_tier: true, fighter_id: turtle.id)
+      
+      expect(move.order_by_top_tier).to eq([top1, top2])
+    end
+  end
 end
